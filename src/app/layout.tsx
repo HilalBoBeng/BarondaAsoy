5f8a0d793e3dbe6d7c0bd90cf5fb110868d8064a
BoBengg ID <hilallengkas1@gmail.com>
2025-08-25 03:44:37 +0000

    lihat itu tidak hitam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" suppressHydrationWarning>
+    <html lang="id" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${ptSans.variable} font-body antialiased bg-muted/40`}>
+      <body className={`${ptSans.variable} font-body antialiased bg-background`}>
         {children}
         <Toaster />
       </body>
