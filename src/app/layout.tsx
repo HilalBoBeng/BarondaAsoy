672eff6abc65c4cff8cfc8f9e128e9790616c23b
BoBengg ID <hilallengkas1@gmail.com>
2025-09-02 01:47:55 +0000

    1. Ini kenapa besar semua tulisannya

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { PT_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -8,12 +7,6 @@ export const metadata: Metadata = {
   title: "Baronda - Siskamling Digital",
   description: "Aplikasi siskamling untuk keamanan lingkungan Anda.",
 };
-
-const ptSans = PT_Sans({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: '--font-sans',
-});
 
 export default function RootLayout({
   children,
@@ -25,9 +18,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${ptSans.variable} font-body antialiased bg-background`}>
+      <body className="font-body antialiased bg-background">
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
