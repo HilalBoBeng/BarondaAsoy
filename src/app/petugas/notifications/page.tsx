c3f4a3ae5b44552c05d00a9c17e150b0e534bb05
BoBengg ID <hilallengkas1@gmail.com>
2025-09-04 07:32:34 +0000

    ada beberapa efek skeleton yang tidak sesuai yang membuat layar lebar sa

--- src/app/petugas/notifications/page.tsx
+++ src/app/petugas/notifications/page.tsx
@@ -318,12 +318,12 @@ export default function NotificationsPetugasPage() {
                     <TableBody>
                       {loading ? (
                         Array.from({ length: 5 }).map((_, i) => (
-                          <TableRow key={i}>
-                            <TableCell><Skeleton className="h-5 w-28" /></TableCell>
+                           <TableRow key={i}>
                             <TableCell><Skeleton className="h-5 w-32" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                             <TableCell><Skeleton className="h-5 w-40" /></TableCell>
-                            <TableCell><Skeleton className="h-8 w-10" /></TableCell>
-                            <TableCell className="text-right"><Skeleton className="h-8 w-10 ml-auto" /></TableCell>
+                            <TableCell><Skeleton className="h-10 w-10" /></TableCell>
+                            <TableCell className="text-right"><Skeleton className="h-10 w-10 ml-auto" /></TableCell>
                           </TableRow>
                         ))
                       ) : notifications.length > 0 ? (
