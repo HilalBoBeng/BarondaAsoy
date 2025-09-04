87f4d8a2943b54627d35cc52fa52daab0d090065
BoBengg ID <hilallengkas1@gmail.com>
2025-09-04 22:23:27 +0000

    Anda liat di foto pada menu "notifikasi" admin/petugas, skeleton awalnya

--- src/app/petugas/notifications/page.tsx
+++ src/app/petugas/notifications/page.tsx
@@ -319,11 +319,11 @@ export default function NotificationsPetugasPage() {
                       {loading ? (
                         Array.from({ length: 5 }).map((_, i) => (
                            <TableRow key={i}>
-                            <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-                            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                            <TableCell><Skeleton className="h-5 w-40" /></TableCell>
-                            <TableCell><Skeleton className="h-10 w-10" /></TableCell>
-                            <TableCell className="text-right"><Skeleton className="h-10 w-10 ml-auto" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell className="w-[100px]"><Skeleton className="h-10 w-10" /></TableCell>
+                            <TableCell className="w-[50px] text-right"><Skeleton className="h-10 w-10 ml-auto" /></TableCell>
                           </TableRow>
                         ))
                       ) : notifications.length > 0 ? (
